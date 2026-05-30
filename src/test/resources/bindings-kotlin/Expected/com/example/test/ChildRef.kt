a8f20000dbee8493e4238ce02b5b5ab196c30e22
liwp <1628208948@qq.com>
2026-05-30 23:11:06 +0800

    refactor(bindings): 实现两种获取引用的方式

--- src/test/resources/bindings-kotlin/Expected/com/example/test/ChildRef.kt
+++ src/test/resources/bindings-kotlin/Expected/com/example/test/ChildRef.kt
@@ -5,12 +5,14 @@ import godot.core.NodePath
 import kotlin.reflect.KProperty
 
 class ChildRef<T : Node>(private val relativePath: String) {
-    operator fun getValue(thisRef: Node, property: KProperty<*>): T {
+    fun get(thisRef: Node): T {
         val node = thisRef.getNode(NodePath(relativePath))
             ?: throw NodeNotFoundException(relativePath)
         @Suppress("UNCHECKED_CAST")
         return node as T
     }
+
+    operator fun getValue(thisRef: Node, property: KProperty<*>): T = get(thisRef)
 }
 
 class NodeNotFoundException(path: String) : Exception("Node not found at path: $path")
